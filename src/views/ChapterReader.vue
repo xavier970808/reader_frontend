b1f071e0deec9852b4896bb75c1791e8ec24d32a
Xavier <xavier970808@gmail.com>
2025-05-26 22:11:39 +0800

    5/26

--- src/views/ChapterReader.vue
+++ src/views/ChapterReader.vue
@@ -11,7 +11,7 @@
           :key="i"
           :title="`第 ${i + 1} 章`"
         >
-          <p style="white-space: pre-wrap" class="text-lg">{{ c }}</p>
+          <div class="text-lg" v-html="c"></div>
         </a-collapse-item>
       </a-collapse>
     </div>
